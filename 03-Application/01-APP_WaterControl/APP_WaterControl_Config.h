d8e35a311416a4c5f675439762827e3700830014
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2026-09-03 20:28:00 +0300

    Add files via upload

---
+++ 03-Application/01-APP_WaterControl/APP_WaterControl_Config.h
@@ -0,0 +1,8 @@
+#ifndef APP_WATERCONTROL_CONFIG_H
+#define APP_WATERCONTROL_CONFIG_H
+
+/* TODO: Define hysteresis thresholds */
+#define WATER_LEVEL_LOW      20  
+#define WATER_LEVEL_HIGH     80  
+
+#endif
